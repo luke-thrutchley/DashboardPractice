025603b93566d87d881242658232feefb7e8cbd9
luke_thrutchley <lthrutch@ttu.edu~>
2024-12-21 15:00:05 -0600

    happy with progress for first day

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from lxml import etree
 
 def fetch_data(page, level_id, sortby):
     try:
@@ -19,7 +20,22 @@ def fetch_data(page, level_id, sortby):
         print("request error")
         return None
 
+def parse_xml(text):
+    print("start parse")
+    root = etree.fromstring(text.encode('utf-8'))
+    print("end parse")
+    return root
+    
+
+def format_top_10(level_id):
+    print("start format")
+
+
 def main():
+    top10 = [
+        []
+    ]
+    t10index = 0
     try:
         level_id = int(input("level_id: "))
     except ValueError:
@@ -28,9 +44,16 @@ def main():
     response = fetch_data(1, level_id, "completion_time")
     if response is None:
         print("Failed to fetch data.")
+        return 1
     else:
-        print("Response received:")
-        print(response)
+        print("Response received")
+        rps = parse_xml(response)
+        for rp in rps:
+            username = rp.attrib.get('un')
+            clear_time = rp.attrib.get('ct')
+            date_of_clear = rp.attrib.get('dt')
+            # save to top 10 if not in hashmap
+            
 
 if __name__ == "__main__":
     main()
